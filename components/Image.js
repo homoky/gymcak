3c8c0bb30316e6defddd67274135134770beb3d3
Petr Homoky <p.homoky@gmail.com>
2021-04-28 16:21:13 +0000

    SDAdas

---
+++ components/Image.js
@@ -0,0 +1,13 @@
+const Image = ({ src, width, isSquare }) => {
+
+    const height = isSquare ? width : "auto"
+
+    return <img 
+            src={src} 
+            style={{width, height}} 
+            />
+}
+
+export default Image
+
+// Typescript
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Link from 'next/link'
 import Button, {greeting, superGreeting} from './../components/Button'
+import Image from './../components/Image'
 
 const HomePage = () => {
+
+    const isSquare = false;
+
     return <div>
         Welcome to Next.js! Go to {" "}
         <Link href="/hello"><a>hello page</a></Link>.
@@ -10,6 +14,7 @@ const HomePage = () => {
         <Button children="Tohle je super tlačítko" onClick={()=>{console.log("ahoj")}} />
         {greeting("Petr")}
         {superGreeting({name: "super greeting name"})}
+        <Image src="https://homoky-files.fra1.digitaloceanspaces.com/2020/nL2ckrgYNz.png" width={300} isSquar />
     </div>
 }
   
