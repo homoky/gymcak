27b9e8c17e5b30b1289dd42f223812d103375a64
Petr Homoky <p.homoky@gmail.com>
2021-05-03 16:02:13 +0000

    Advanced counter

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Link from 'next/link'
-import Button, {greeting, superGreeting} from './../components/Button'
-import Image from './../components/Image'
+import Counter from './../components/Counter'
 
 const HomePage = () => {
-
-    const isSquare = false;
-
     return <div>
         Welcome to Next.jss! Go to {" "}
-        <Image src="https://homoky-files.fra1.digitaloceanspaces.com/2020/nL2ckrgYNz.png" width={300} isSquar />
+        <Counter defaultValue={5} incrementor={5} />
+        <Counter incrementor={10} />
+        <Counter defaultValue={100} />
+        <Counter />
+        <Counter />
+        <Counter />
     </div>
 }
   
