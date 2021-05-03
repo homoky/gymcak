0949364c1ebbfb469fe3cd17a583a6ac996017f7
Petr Homoky <p.homoky@gmail.com>
2021-05-03 15:38:00 +0000

    Updates

--- pages/hello.js
+++ pages/hello.js
@@ -8,7 +8,7 @@ export async function getServerSideProps(context) {
     const res = await axios.get(`https://jsonplaceholder.typicode.com/todos/1`)
     const data = res.data;
 
-    console.log("Dotaz do databáze.")
+    console.log("Dotaz do databáze po každém načtení v prohlížeči.")
   
     return {
       props: {title: data.title},
--- pages/hello2.js
+++ pages/hello2.js
@@ -8,7 +8,7 @@ export async function getStaticProps(context) {
     const res = await axios.get(`https://jsonplaceholder.typicode.com/todos/1`)
     const data = res.data;
 
-    console.log("Dotaz do databáze.")
+    console.log("Dotaz do databáze jednou při buildu.")
   
     return {
       props: {title: data.title},
